48dacbe36f9b9bc76d0bc1e7b4b0e4f32a0c264b
fore447 <mmori.0426@gmail.com>
2021-12-20 01:08:56 +0900

    update file

--- C-Sequence.py
+++ C-Sequence.py
@@ -1,32 +1,19 @@
-# +, -, +, -, + の時
-def posi_nega(arr, arr_length):
+# turn=0の時　-, +, -, +, -
+# turn=1の時　+, -, +, -, +
+def culc_min(arr, tmp_sum, arr_length, turn):
     cnt = 0
-    for i in range(n):
-        if i % 2 == 0:
-            if sum(arr[0:i+1]) < 0:
-                cnt += abs(arr[i]-(-sum(arr[0:i])+1))
-                arr[i] = -sum(arr[0:i])+1
+    for i in range(arr_length):
+        # マイナス計算
+        if (i+turn) % 2 == 0:
+            if tmp_sum+arr[i] >= 0:
+                cnt += abs(arr[i]-(-tmp_sum-1))
+                arr[i] = -tmp_sum-1
+        #プラス計算
         else:
-            if sum(arr[0:i+1]) > 0:
-                cnt += abs(arr[i]-(-sum(arr[0:i])-1))
-                arr[i] = -sum(arr[0:i])-1
-
-        if i==arr_length-1 and sum(arr[0:arr_length]) == 0:
-            cnt += 1
-    return cnt
-
-# -, +, -, +, - の時
-def nega_posi(arr, arr_length):
-    cnt = 0
-    for i in range(n):
-        if i % 2 == 0:
-            if sum(arr[0:i+1]) > 0:
-                cnt += abs(arr[i]-(-sum(arr[0:i])-1))
-                arr[i] = -sum(arr[0:i])-1
-        else:
-            if sum(arr[0:i+1]) < 0:
-                cnt += abs(arr[i]-(-sum(arr[0:i])+1))
-                arr[i] = -sum(arr[0:i])+1
+            if tmp_sum+arr[i] <= 0:
+                cnt += abs(arr[i]-(-tmp_sum+1))
+                arr[i] = -tmp_sum+1
+        tmp_sum += arr[i]
 
         if i==arr_length-1 and sum(arr[0:arr_length]) == 0:
             cnt += 1
@@ -36,7 +23,10 @@ if __name__ == '__main__':
     n = int(input().rstrip())
     a = list(map(int, input().rstrip().split()))
     tmp_a = a.copy()
-    posi_nega_cnt = posi_nega(a, n)
-    nega_posi_cnt = nega_posi(tmp_a, n)
+
+    # turn=0の時　-, +, -, +, -
+    # turn=1の時　+, -, +, -, +
+    nega_posi_cnt = culc_min(a, 0, n, 0)
+    posi_nega_cnt = culc_min(tmp_a, 0, n, 1)
 
     print(min(posi_nega_cnt, nega_posi_cnt))
