425e0dabbcda129ac0113a07dae10c243460915b
fore447 <mmori.0426@gmail.com>
2022-01-19 00:36:02 +0900

    update file

--- C_counting2.py
+++ C_counting2.py
@@ -9,3 +9,25 @@ for i in range(Q):
 
 for i in Ans:
     print(i)
+
+# N, Q = map(int, input().rstrip().split())
+# A = sorted(list(map(int, input().rstrip().split())))
+
+# def binary_search(data, value):
+#     ok = len(data)
+#     ng = -1
+#     while ok - ng > 1:
+#         mid = (ok + ng) // 2
+#         if data[mid] >= value:
+#             ok = mid
+#         elif data[mid] < value:
+#             ng = mid
+#     return ok
+
+# Ans = []
+# for i in range(Q):
+#     x = int(input().rstrip())
+#     Ans.append(N - binary_search(A, x))
+
+# for i in Ans:
+#     print(i)
